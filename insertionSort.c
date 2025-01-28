34e6aa44aa985ba63954b575fa34beca5933ee80
=sankethl27 <sankethlreddy@gmail.com>
2025-01-28 23:09:24 +0530

    Insertion Sort on LinkedList

---
+++ insertionSort.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct singlyLinkedList
+{
+    int data;
+    struct singlyLinkedList *next;
+} node;
+
+node *createNode(int value)
+{
+    node *newNode = (node *)malloc(sizeof(node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void swap(node *first, node *second)
+{
+    int temp = first->data;
+    first->data = second->data;
+    second->data = temp;
+}
+
+void printLinkedList(node *head)
+{
+    node *current = head;
+    while (current != NULL)
+    {
+        printf("%d ", current->data);
+        current = current->next;
+    }
+    printf("\n");
+}
+
+void insertionSortLinkedList(node **head)
+{
+    if (*head == NULL)
+        return;
+
+    node *sorted = NULL;
+    node *current = *head;
+
+    while (current != NULL)
+    {
+        node *next = current->next;
+        if (sorted == NULL || sorted->data >= current->data)
+        {
+            current->next = sorted;
+            sorted = current;
+        }
+        else
+        {
+            node *temp = sorted;
+            while (temp->next != NULL && temp->next->data < current->data)
+            {
+                temp = temp->next;
+            }
+            current->next = temp->next;
+            temp->next = current;
+        }
+
+        current = next;
+    }
+
+    *head = sorted;
+}
+
+node *createLinkedList()
+{
+    node *head = NULL;
+    node *current = NULL;
+    int number;
+
+    while (scanf("%d", &number) == 1)
+    {
+        if (head == NULL)
+        {
+            current = createNode(number);
+            head = current;
+        }
+        else
+        {
+            current->next = createNode(number);
+            current = current->next;
+        }
+        if (getchar() == '\n')
+        {
+            break;
+        }
+    }
+    return head;
+}
+
+int main()
+{
+    node *head = createLinkedList();
+
+    insertionSortLinkedList(&head);
+    printf(" elements are: ");
+    printLinkedList(head);
+    return 0;
+}
